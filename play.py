b7407fc55229cbe96b652792b456c6ee7bcec505
FeederYap <feederyap@gmail.com>
2023-04-08 16:40:49 +0100

    add traning dqn py code

---
+++ play.py
@@ -0,0 +1,126 @@
+from mss import mss
+import pydirectinput
+import cv2
+import numpy as np
+import pytesseract
+from matplotlib import pyplot as plt
+import time
+from gym import Env
+from gym.spaces import Box, Discrete
+
+class WebGame(Env):
+    def __init__(self) -> None:
+        super().__init__()
+        self.observation_space = Box(low=0, high=255, shape=(1,83,100), dtype=np.uint8)
+        self.action_space = Discrete(3)
+        self.cap = mss()
+        self.game_location = {'top': 580, 'left': 50, 'width': 600, 'height': 500}
+        self.done_location = {'top': 685, 'left': 865, 'width': 660, 'height': 70}
+
+    def step(self, action):
+        action_map = {
+            0:'space',
+            1: 'down', 
+            2: 'no_op'
+        }
+        if action !=2:
+            pydirectinput.press(action_map[action])
+
+        done, done_cap = self.get_done() 
+        observation = self.get_observation()
+        reward = 1 
+        info = {}
+        return observation, reward, done, info
+    
+    def render(self):
+        cv2.imshow('Game', np.array(self.cap.grab(self.game_location))[:,:,:3])
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.close()
+
+    def close(self):
+        cv2.destroyAllWindows()
+ 
+    def reset(self):
+        time.sleep(1)
+        pydirectinput.click(x=150, y=150)
+        pydirectinput.press('space')
+        return self.get_observation()
+    
+    def get_observation(self):
+        raw = np.array(self.cap.grab(self.game_location))[:,:,:3].astype(np.uint8)
+        gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+        resized = cv2.resize(gray, (100,83))
+        channel = np.reshape(resized, (1,83,100))
+        return channel
+
+    def get_done(self):
+        done_cap = np.array(self.cap.grab(self.done_location))
+        done_strings = ['GAME', 'GAHE']
+        done = False
+        res = pytesseract.image_to_string(done_cap)[:4]
+        if res in done_strings:
+            done = True
+        return done, done_cap
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# env = WebGame()
+# for episode in range(10): 
+#     obs = env.reset()
+#     done = False  
+#     total_reward   = 0
+#     while not done: 
+#         obs, reward,  done, info =  env.step(env.action_space.sample())
+#         total_reward  += reward
+#     print('Total Reward for episode {} is {}'.format(episode, total_reward))
+
+# Import os for file path management
+import os 
+# Import Base Callback for saving models
+from stable_baselines3.common.callbacks import BaseCallback
+# Check Environment    
+from stable_baselines3.common import env_checker
+
+# env_checker.check_env(env)
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+    
+# env_checker.check_env(env)
+CHECKPOINT_DIR = './train/'
+LOG_DIR = './logs/'
+callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
+from stable_baselines3 import DQN
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+pydirectinput.FAILSAFE = False
+env = WebGame()
+# model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=100000, learning_starts=1000)
+model = DQN.load('./train/best_model_100000')
+# model.learn(total_timesteps=100000, callback=callback)
+
+# model.load('train_first/best_model_50000') 
+for episode in range(5): 
+    obs = env.reset()
+    done = False
+    total_reward = 0
+    while not done: 
+        action, _ = model.predict(obs)
+        obs, reward, done, info = env.step(int(action))
+        # time.sleep(0.01)
+        total_reward += reward
+    print('Total Reward for episode {} is {}'.format(episode, total_reward))
+    # time.sleep(2)        
